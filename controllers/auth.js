c5cd175f0d76eb4aa8fd4ac028f49720572ce415
artzamukie001 <sarawut_boon@elearning.cmu.ac.th>
2025-06-17 19:27:46 +0700

    feat: create register api

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,24 @@
 const prisma = require("../config/prisma");
+const bcrypt = require("bcrypt");
 
-exports.register = (req, res) => {
+exports.register = async (req, res) => {
   try {
-    res.json({ message: "Hello register!" });
+    const { name, email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (user)
+      return res.status(400).json({ message: "Email already exist!!!" });
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: { name, email, password: hashPassword },
+    });
+
+    res.json({ message: "Register successfully!" });
   } catch (error) {
     res
       .status(500)
---
+++ prisma/migrations/20250617122424_init2/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comment" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
