eaa09ed25b80bf2e105ee0ce7ab05951f85675fc
artzamukie001 <sarawut_boon@elearning.cmu.ac.th>
2025-06-17 19:51:13 +0700

    feat: create login api

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const prisma = require("../config/prisma");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
@@ -19,6 +20,43 @@ exports.register = async (req, res) => {
     });
 
     res.json({ message: "Register successfully!" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong, Please try again!!!" });
+    console.error(error);
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) return res.status(400).json({ message: "User not found!!!" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Email or Password invalid!!!" });
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+    };
+
+    jwt.sign(
+      payload,
+      process.env.SECRET_KEY,
+      { expiresIn: "1d" },
+      (err, token) => {
+        if (err) return res.status(400).json({ message: "Token invalid!!!" });
+        res.json({ payload, token });
+      }
+    );
   } catch (error) {
     res
       .status(500)
