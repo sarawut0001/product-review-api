d1978812abbe987d06c5fd17b52abfa32b7937ab
artzamukie001 <sarawut_boon@elearning.cmu.ac.th>
2025-06-17 21:18:45 +0700

    feat: create get product with review api

--- controllers/product.js
+++ controllers/product.js
@@ -1,10 +1,53 @@
 const prisma = require("../config/prisma");
 
+exports.createProduct = async (req, res) => {
+  try {
+    const { name, description, price, imageUrl } = req.body;
+
+    await prisma.product.create({
+      data: { name, description, price, imageUrl },
+    });
+
+    res.json({ message: "Create successfully!" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong, Please try again!!!" });
+    console.error(error);
+  }
+};
+
 exports.getProducts = async (req, res) => {
   try {
     const products = await prisma.product.findMany();
 
     res.json(products);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong, Please try again!!!" });
+    console.error(error);
+  }
+};
+
+exports.getProductWithReview = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const product = await prisma.product.findUnique({
+      where: { id },
+      include: {
+        review: {
+          include: {
+            user: {
+              select: { id: true, name: true },
+            },
+          },
+        },
+      },
+    });
+
+    res.json(product);
   } catch (error) {
     res
       .status(500)
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts } = require("../controllers/product");
+const {
+  createProduct,
+  getProducts,
+  getProductWithReview,
+} = require("../controllers/product");
+const { authCheck, adminCheck } = require("../middleware/authCheck");
 
+router.post("/product", authCheck, adminCheck, createProduct);
 router.get("/products", getProducts);
+router.get("/product/:id", getProductWithReview);
 
 module.exports = router;
